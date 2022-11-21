15d1682a5686e160f36d83a7312f98c765e571bd
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-11-21 16:50:29 -0500

    update default urls

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -2,10 +2,8 @@ import { defineStore } from 'pinia'
 
 export const useSettingsStore = defineStore('settings', {
   state: () => ({
-    lichessHost:
-      'https://8080-lichessorg-lilagitpod-15zotrnb8h8.ws-us73.gitpod.io',
-    externalEngineHost:
-      'https://9666-lichessorg-lilagitpod-15zotrnb8h8.ws-us73.gitpod.io',
+    lichessHost: 'https://lichess.org',
+    externalEngineHost: 'https://engine.lichess.ovh',
   }),
   persist: true,
 })
