2df480c7da663108be7310bc3697b54676634b47
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-04-14 23:14:18 -0400

    dashboard wip

---
+++ src/components/Chessboard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Chessground } from 'chessground'
+import { onMounted, ref } from 'vue';
+
+const boardElement = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+    if (boardElement.value) {
+        const ground = Chessground(boardElement.value, {
+          viewOnly: true,
+        })
+    }
+})
+</script>
+
+<style>
+.chessboard {
+  width: 480px;
+  height: 480px;
+  opacity: 0.5;
+}
+
+.chessboard cg-board {
+  background-color: #e1e6ea;
+}
+</style>
+
+<template>
+    <div ref="boardElement" class="chessboard"></div>
+</template>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,6 +2,7 @@
 import { useEventLogsStore } from '../stores/event-logs'
 import PageTitle from './PageTitle.vue'
 import GettingStarted from './GettingStarted.vue'
+import Chessboard from './Chessboard.vue'
 import { useSettingsStore } from '../stores/settings'
 import { useEnginesStore } from '../stores/engines'
 
@@ -17,6 +18,25 @@ const eventLogs = useEventLogsStore()
     <PageTitle>Dashboard</PageTitle>
 
     <div class="page-content">
+      <Chessboard></Chessboard>
+
+      <ul>
+        <li>evaluation</li>
+        <li>lowerbound|upperbound</li>
+        <li>depth</li>
+        <li>seldepth</li>
+        <li>nodes</li>
+        <li>nodes per second</li>
+        <li>hashfull</li>
+        <li>tbhits</li>
+        <li>time</li>
+      </ul>
+
+      <h3>Status</h3>
+      <p>waiting for move...</p>
+      <p>analyzing...</p>
+      <p>sleeping for n seconds...</p>
+
       <div
         class="bg-white p-4 h-1/2 flex flex-col-reverse justify-end overflow-scroll font-mono"
       >
