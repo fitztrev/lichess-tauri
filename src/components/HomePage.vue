83fd9f5f46d02d5b42955a135d308cbe10c978a1
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-04-16 11:20:57 -0400

    dashboard ui cleanup

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -24,41 +24,47 @@ const analysis = useAnalysisStore()
         <Chessboard :fen="analysis.fen"></Chessboard>
       </div>
       <div class="flex-initial w-1/3 text-center">
-        <h3 class="text-4xl">{{ analysis.evaluation }}</h3>
-        <p>
-          {{ analysis.uci.scoreType }}
-          {{ analysis.uci.scoreValue }}
-          {{ analysis.uci.scoreBound }}
-        </p>
+        <h3 class="text-5xl font-bold">{{ analysis.evaluation }}</h3>
         <p>Evaluation</p>
 
-        <h3 class="mt-2 text-xl">{{ analysis.uci.depth }}</h3>
-        <p>Depth</p>
+        <div class="flex">
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.uci.depth }}</h3>
+            <div class="text-gray-600">Depth</div>
+          </div>
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.uci.seldepth }}</h3>
+            <div class="text-gray-600">Sel Depth</div>
+          </div>
+        </div>
 
-        <h3 class="mt-2 text-xl">{{ analysis.uci.seldepth }}</h3>
-        <p>Sel Depth</p>
+        <div class="flex">
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.nodes }}</h3>
+            <div class="text-gray-600">Nodes</div>
+          </div>
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.nps }}</h3>
+            <div class="text-gray-600">Nodes/sec</div>
+          </div>
+        </div>
 
-        <h3 class="mt-2 text-xl">{{ analysis.nodes }}</h3>
-        <p>Nodes</p>
-
-        <h3 class="mt-2 text-xl">{{ analysis.nps }}</h3>
-        <p>Nodes/sec</p>
-
-        <h3 class="mt-2 text-xl">{{ analysis.time }}</h3>
-        <p>Time</p>
-
-        <h3 class="mt-2 text-xl">{{ analysis.hashUsage }}</h3>
-        <p>Hash Usage %</p>
-
-        <h3 class="mt-2 text-xl">{{ analysis.uci.tbhits }}</h3>
-        <p>Tablebase Hits</p>
+        <div class="flex">
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.time }}</h3>
+            <div class="text-gray-600">Time</div>
+          </div>
+          <div class="flex-initial w-1/2">
+            <h3 class="mt-2 text-3xl">{{ analysis.hashUsage }}</h3>
+            <div class="text-gray-600">Hash Usage %</div>
+          </div>
+        </div>
 
         <h3 class="mt-8 text-lg">{{ analysis.status }}</h3>
         <SleepCountdown
           v-if="analysis.sleepDuration > 0"
           :duration="analysis.sleepDuration"
         />
-        <p>Status</p>
       </div>
     </div>
   </template>
