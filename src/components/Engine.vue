b1a83c7030feeb5df529e9ab9663b7f5068a719c
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-04-16 13:16:59 -0400

    fix nav icon centering

--- src/components/Engine.vue
+++ src/components/Engine.vue
@@ -16,34 +16,28 @@ function deleteEngine(): void {
 </script>
 
 <template>
-  <a href="#" class="block hover:bg-gray-50">
-    <div class="px-4 py-4 sm:px-6">
-      <div class="flex items-center justify-between">
-        <p class="truncate text-sm font-medium text-indigo-600">
-          {{ engine.name }}
-        </p>
-        <div class="ml-2 flex flex-shrink-0">
-          <p
-            v-for="variant in engine.variants"
-            class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800"
-          >
-            {{ variant }}
-          </p>
-        </div>
-      </div>
-      <div class="mt-2 sm:flex sm:justify-between">
-        <div class="sm:flex">
-          <p class="flex items-center text-sm text-gray-500">
-            {{ engine.maxThreads }} threads &bullet;
-            {{ memoryToHumanReadable(engine.maxHash) }} &bullet;
-            {{ engine.defaultDepth }} depth
-          </p>
+  <div class="px-4 py-4 sm:px-6">
+    <div class="flex items-center justify-between">
+      <p class="truncate text-sm font-medium text-indigo-600">
+        {{ engine.name }}
+      </p>
+      <div class="ml-2 flex flex-shrink-0">
+        <div>
+          <a href="#" @click.prevent="deleteEngine">Delete</a>
+          <router-link :to="{ name: 'editEngine', params: { id: engine.id } }">
+            Edit
+          </router-link>
         </div>
       </div>
     </div>
-  </a>
-  <a href="#" @click.prevent="deleteEngine">Delete</a>
-  <router-link :to="{ name: 'editEngine', params: { id: engine.id } }">
-    Edit
-  </router-link>
+    <div class="mt-2 sm:flex sm:justify-between">
+      <div class="sm:flex">
+        <p class="flex items-center text-sm text-gray-500">
+          {{ engine.maxThreads }} threads &bullet;
+          {{ memoryToHumanReadable(engine.maxHash) }} &bullet;
+          {{ engine.defaultDepth }} depth
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
