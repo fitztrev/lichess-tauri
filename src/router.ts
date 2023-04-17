7b385b3fd6f6fc34ba8fa0d7a47e2523a5f2f214
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-04-16 21:17:50 -0400

    rename homepage component to dashboard

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import SettingsPage from './components/SettingsPage.vue'
-import HomePage from './components/HomePage.vue'
+import Dashboard from './components/Dashboard.vue'
 import EngineForm from './components/EngineForm.vue'
 import EnginesPage from './components/EnginesPage.vue'
 
 const routes = [
-  { path: '/', component: HomePage },
+  { path: '/', component: Dashboard },
   { path: '/engines', component: EnginesPage },
   { path: '/engines/new', component: EngineForm },
   { path: '/engines/edit/:id', component: EngineForm, name: 'editEngine' },
