be4c7377757f224e60d0e9a23ef6f4286bc6e46a
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-08-23 08:18:37 -0400

    Move oauth handling from frontend to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,10 +5,10 @@
 
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use engine_directory::Engine;
+use reqwest::Url;
 use serde_json::{json, Value};
-use std::{borrow::Cow, sync::Arc, thread};
+use std::{borrow::Cow, fmt::format, thread};
 use sysinfo::{CpuExt, System, SystemExt};
-use tauri::Window;
 use tauri_plugin_oauth::OauthConfig;
 
 use crate::db::establish_connection;
@@ -53,6 +53,11 @@ fn delete_engine(engine_id: &str) {
 }
 
 #[tauri::command]
+fn open_path(path: String) {
+    utils::open_path(path);
+}
+
+#[tauri::command]
 fn get_sysinfo() -> Value {
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -77,10 +82,16 @@ fn get_sysinfo() -> Value {
     })
 }
 
+// #[derive(serde::Deserialize, Debug)]
+// struct AccessTokenResponse {
+//     token_type: String,
+//     access_token: String,
+//     expires_in: i32,
+// }
+
 #[tauri::command]
-async fn start_oauth_server(window: Window) {
-    let window_arc = Arc::new(window);
-    let window_arc2 = window_arc.clone();
+async fn start_oauth_server() {
+    let (code_challenge, code_verify) = oauth2::PkceCodeChallenge::new_random_sha256();
 
     let port = tauri_plugin_oauth::start_with_config(
         OauthConfig {
@@ -88,13 +99,52 @@ async fn start_oauth_server(window: Window) {
             response: Some(Cow::Borrowed(include_str!("../public/oauth_response.html"))),
         },
         move |url| {
-            window_arc2.emit("returning_from_lichess", url).unwrap();
+            println!("returning_from_lichess: {}", url);
+
+            let url = Url::parse(&url).unwrap();
+            println!("url: {:?}", url);
+
+            let code = url.query_pairs().find(|(key, _)| key == "code").unwrap().1;
+
+            println!("code_verify: {}", code_verify.secret());
+
+            let lichess_host = db::get_setting("lichess_host").unwrap();
+
+            let body = reqwest::blocking::Client::new()
+                .post(format!("{}/api/oauth", lichess_host))
+                .form(&[
+                    ("grant_type", "authorization_code"),
+                    ("client_id", "github.com/fitztrev/lichess-tauri"),
+                    ("code", code.to_string().as_str()),
+                    (
+                        "redirect_uri",
+                        format!("http://localhost:{}/", url.port().unwrap()).as_str(),
+                    ),
+                    ("code_verifier", code_verify.secret()),
+                ])
+                .send()
+                .unwrap()
+                .json::<serde_json::Value>()
+                .unwrap();
+
+            println!("body: {:?}", body);
         },
     )
     .unwrap();
 
     println!("Local server started on port: {}", port);
-    window_arc.emit("server_started", port).unwrap();
+    let redirect_url = format!("http://localhost:{}/", port);
+
+    let lichess_host = db::get_setting("lichess_host").unwrap();
+    let url = format!(
+        "{}/oauth?response_type=code&client_id={}&redirect_uri={}&code_challenge_method=S256&code_challenge={}&scope=engine:read%20engine:write",
+        lichess_host,
+        "github.com/fitztrev/lichess-tauri",
+        redirect_url,
+        code_challenge.as_str()
+    );
+
+    open_path(url);
 }
 
 #[tauri::command]
@@ -129,6 +179,7 @@ fn main() {
             get_sysinfo,
             start_oauth_server,
             update_setting,
+            open_path
         ])
         .setup(|app| {
             let app_handle = app.handle();
